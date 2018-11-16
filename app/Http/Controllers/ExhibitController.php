40780e9cac54b0304d33f1439e93cab6583641b3
Ben Patterson <bmizepatterson@gmail.com>
2018-11-16 10:30:01 -0500

    Create Exhibit resource

---
+++ app/Http/Controllers/ExhibitController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Exhibit;
+use Illuminate\Http\Request;
+
+class ExhibitController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Exhibit  $exhibit
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Exhibit $exhibit)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Exhibit  $exhibit
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Exhibit $exhibit)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Exhibit  $exhibit
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Exhibit $exhibit)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Exhibit  $exhibit
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Exhibit $exhibit)
+    {
+        //
+    }
+}
