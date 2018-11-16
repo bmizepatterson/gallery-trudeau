de9dc5daf21a706ec01484d7a46e710945cd3c72
Ben Patterson <bmizepatterson@gmail.com>
2018-11-16 14:51:05 -0500

    Fix brand on navbar

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,8 +7,8 @@
 @section('body')
 <nav class="navbar navbar-expand-md navbar-dark">
     <div class="container">
-        <a class="navbar-brand" href="{{ url('/') }}">
-            <span class="brand gallery">gallery</span> <span class="brand trudeau">Trudeau</span>
+        <a class="navbar-brand brand" href="{{ url('/') }}">
+            gallery Trudeau
         </a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
             <span class="navbar-toggler-icon"></span>
